f20fc44dc716b6bc251ff9681d5ef949bc0f21bf
Dinindu Lokugamage <dinindu961030@gmail.com>
2023-07-03 00:43:11 +0530

    Add BookHttpController.ts

---
+++ back-end-nodejs/src/main.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import {router as BookHttpController} from "./api/BookHttpController";
+
+
+const app=express();
+
+app.use('api/v1/books',BookHttpController);
+
+app.listen(8081, () => console.log("Server has been started at 8081"));
